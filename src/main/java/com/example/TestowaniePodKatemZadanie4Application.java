384281564e28426c86235eb9eaaa32aeff31f77e
Damian <Damian@DESKTOP-UM09VT4>
2016-11-29 22:07:46 +0100

    Nowe zadanie

---
+++ src/main/java/com/example/TestowaniePodKatemZadanie4Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestowaniePodKatemZadanie4Application 
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(TestowaniePodKatemZadanie4Application.class, args);
+	}
+}
